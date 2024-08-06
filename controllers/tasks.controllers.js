e81e4c9defa982638152adbd04d00198f9734be9
Maurcio Heredia <maurih46322945@gmail.com>
2024-08-06 11:35:09 -0300

    Se creo el GET para obtener tarea por su id

--- controllers/tasks.controllers.js
+++ controllers/tasks.controllers.js
@@ -19,4 +19,30 @@ const getAllTasks = (req, res) => {
     });
 };
 
-module.exports = { getAllTasks };
+const getTaskById = (req, res) => {
+    const conexion = getConnection();
+    const { id } = req.params;
+    conexion.connect(err => {
+        if (err) {
+            console.error('Error conectando a la base de datos:', err);
+            return res.status(500).send('Error conectando a la base de datos');
+        }
+
+        conexion.query('SELECT * FROM tasks WHERE id = ?', [id], (err, results) => {
+            if (err) {
+                console.error('Error al obtener la tarea:', err);
+                return res.status(500).send('Error al obtener la tarea');
+            }
+            if (results.length === 0) {
+                return res.status(404).send('Tarea no encontrada');
+            }
+            res.json(results[0]);
+            conexion.end();
+        });
+    });
+};
+
+module.exports = { 
+    getAllTasks,
+    getTaskById,
+ };
--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const {
-    getAllTasks
+    getAllTasks,
+    getTaskById,
 
 } = require('../controllers/tasks.controllers');
 
 router.get('/tasks', getAllTasks);
+router.get('/task/:id', getTaskById);
 
 
 module.exports = router;
